c9d13df2edc028bdb414de80fa2d535b9f10b6e6
Robert Silva <robert.f.silva19@gmail.com>
2024-01-25 01:59:27 -0300

    Criação de Componentes
    - criação de componentes Navbar, Produto-Vitrine.
    - ajuste de layout e botões.

---
+++ src/components/produto-vitrine/produto-vitrine.jsx
@@ -0,0 +1,24 @@
+import "./produto-vitrine.css";
+import foto from "../../assets/hamburguer.png";
+import bag from "../../assets/bag-black.png";
+
+
+function ProdutoVitrine(){
+    return <div className="produto-box text-center">
+        <img src={foto} alt="Foto" />
+        <div>
+            <h2>Cheese Burguer</h2>
+            <p className="prod-vitrine-descricao">Hamburguer de 250g, queijo, tomate, alface e cebola.</p>
+            <p className="prod-vitrine-preco">R$ 18,90</p>
+        </div>
+
+        <div>
+            <button className="btn btn-cart">
+                <img src={bag} className="icon"/>
+                Adicionar
+            </button>
+        </div>
+    </div>
+}
+
+export default ProdutoVitrine;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,28 @@
+import Navbar from "../../components/navbar/navbar.jsx";
+import ProdutoVitrine from "../../components/produto-vitrine/produto-vitrine.jsx";
+
 function Home(){
-    return <h1>Home...</h1>
+    return <>
+        <div>
+            <Navbar/>
+
+            <div className="container">
+                <div className="titulo text-center">
+                    <h1>Nosso Cardápio</h1>
+                    <p className="subtitulo">Clique em adicionar para colocar os produtos na sacola de compras.
+                        Se preferir, você pode pedir pelo WhatsApp: (61) 98361-0826.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div className="text-center">
+            <ProdutoVitrine/>
+            <ProdutoVitrine/>
+            <ProdutoVitrine/>
+            <ProdutoVitrine/>
+        </div>
+    </>
 }
 
 export default Home;
